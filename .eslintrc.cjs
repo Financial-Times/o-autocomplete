54bd7a6d09b56335d576e52208e6b4c08802a827
chee <chee@snoot.club>
2021-10-06 18:01:25 +0100

    Add note about The Origami Component System

--- .eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"extends": "origami-component",
-	"parserOptions": {
-		"ecmaVersion": 2021,
-		"sourceType": "module"
-	}
-};
--- main.js
+++
@@ -1,7 +0,0 @@
-import oAutocomplete from './src/js/autocomplete.js';
-const constructAll = function () {
-	oAutocomplete.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
-document.addEventListener('o.DOMContentLoaded', constructAll);
-export default oAutocomplete;
